51711ca41af5021e51505ceb289c6fbb7801f16d
Robin Gueldenpfennig <mail@robin-gueldenpfennig.de>
2018-01-26 21:36:18 +0100

    add OneDirectionWalkway prefab

---
+++ Assets/Game/Scripts/Walkway.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Walkway : MonoBehaviour {
+
+    public float WalkwaySpeed = 5.0f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Collision occured with " + collision.transform.name);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("package"))
+        {
+            collision.rigidbody.velocity = transform.forward * WalkwaySpeed;
+            //collision.rigidbody.AddForce(transform.forward * WalkwaySpeed, ForceMode.Acceleration);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        print("No longer in contact with " + collision.transform.name);
+    }
+}
