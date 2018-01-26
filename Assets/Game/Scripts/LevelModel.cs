e752f1dc87d079363b491c48b4f5f38c05786750
Manuel Krebber <admin@wheerd.de>
2018-01-27 00:16:12 +0100

    Add timer to level UI

--- Assets/Game/Scripts/LevelModel.cs
+++ Assets/Game/Scripts/LevelModel.cs
@@ -1,19 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 using Zenject;
 
 public class LevelModel : IInitializable
 {
     public int PackageCount { get; private set; }
 
+    public float Timer { get; private set; }
+
     public void IncrementPackageCount()
     {
         PackageCount++;
     }
 
+    public void IncrementTime()
+    {
+        Timer += Time.deltaTime;
+    }
+
     public void Initialize()
     {
+        ResetLevel();
+    }
+
+    public void ResetLevel()
+    {
         PackageCount = 0;
+        Timer = 0;
     }
 }
---
+++ Assets/Game/Scripts/UI/LevelTimer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class LevelTimer : MonoBehaviour
+{
+    [Inject]
+    private LevelModel _levelModel;
+    
+	void Start () {
+		_levelModel.ResetLevel();
+	}
+	
+	void Update ()
+	{
+        _levelModel.IncrementTime();
+
+	    var totalSeconds = (int) _levelModel.Timer;
+	    var minutes = totalSeconds / 60;
+	    var seconds = totalSeconds % 60;
+
+	    GetComponent<Text>().text = string.Format("{0:D}:{1:00}", minutes, seconds);
+	}
+}
