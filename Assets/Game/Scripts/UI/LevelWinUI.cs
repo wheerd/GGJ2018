5ab866464d8f57aab0cb8338fd38b1030d1696a7
Marco Behnke <marco.behnke@innogames.com>
2018-01-27 10:44:09 +0100

    prepare level win UI

--- Assets/Game/Scripts/UI/LevelWinUI.cs
+++ Assets/Game/Scripts/UI/LevelWinUI.cs
@@ -1,14 +1,21 @@
 ﻿using de.deichkrieger.stateMachine;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
-public class LevelWinUI : MonoBehaviour {
+public class LevelWinUI : MonoBehaviour
+{
 
+	[SerializeField] private Text _packets;
+	[SerializeField] private Text _time;
+	
 	[Inject]
 	GameModel _gameModel;
 	
 	[Inject]
 	GameConfig _gameConfig;
+
+	[Inject] private LevelModel _levelModel;
 	
 	[Inject]
 	private LevelNumberStartSignal _levelStartSignal;
@@ -16,6 +23,12 @@ public class LevelWinUI : MonoBehaviour {
 	[Inject]
 	private LevelChoseSignal _levelChoseSignal;
 
+	void Start()
+	{
+		_packets.text = _levelModel.CorrectPackageCount.ToString() + " / " + _levelModel.ExpectedPackageCount;
+		_time.text = GetTimeFormatted(_levelModel.Timer);
+	}
+	
 	public void StartNextLevel()
 	{
 		if (_gameModel.GetLastLevel() >= _gameConfig.MaxLevel)
@@ -26,5 +39,14 @@ public class LevelWinUI : MonoBehaviour {
 		{
 			_levelStartSignal.Fire(_gameModel.GetLastLevel() + 1);
 		}
+	}
+
+	private string GetTimeFormatted(float time)
+	{
+		var totalSeconds = (int) time;
+		var minutes = totalSeconds / 60;
+		var seconds = totalSeconds % 60;
+
+		return string.Format("{0:D}:{1:00}", minutes, seconds);
 	}
 }
