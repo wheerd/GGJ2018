d8739dfb1451ab85a38191f8509ed312f2196be3
Manuel Krebber <admin@wheerd.de>
2018-01-27 01:33:14 +0100

    Add configurable sequence to spawner

--- Assets/Game/Scripts/Package.cs
+++ Assets/Game/Scripts/Package.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using ModestTree;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Package : MonoBehaviour
 {
     private float _maxSpeed;
 
+    public Color Color;
+
 	void Start ()
 	{
 	    _maxSpeed = 0;
+        transform.localScale.Scale(new Vector3(
+            Random.Range(0.9f, 1.1f),
+            Random.Range(0.9f, 1.1f),
+            Random.Range(0.9f, 1.1f)));
 
+	    GetComponent<MeshRenderer>().material.color = Color;
 	}
 
     public void UpdateMaxBeltSpeed(float speed)
--- Assets/Game/Scripts/Spawner.cs
+++ Assets/Game/Scripts/Spawner.cs
@@ -1,34 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    private float _elapsed = 0;
+    [Serializable]
+    public struct PackageSpawn
+    {
+        public float TimeBefore;
+
+        public Color Color;
+    }
+
+    private int _index;
+
+    private float _elapsed;
 
     public float Speed = 5;
 
-    public float SpawnRate = 0.5f;
+    public float RepeatTime = 0;
 
     public GameObject Package;
+
+    public List<PackageSpawn> PackageSpawns = new List<PackageSpawn>();
     
     void Start ()
-	{
-	    _elapsed = 0;
+    {
+        _index = 0;
+        _elapsed = 0;
 	}
 	
 	void Update ()
 	{
 	    _elapsed += Time.deltaTime;
 
-	    while (_elapsed > SpawnRate)
+	    if (_index == PackageSpawns.Count)
 	    {
-	        _elapsed -= SpawnRate;
-            
-	        var position = transform.position + transform.up * 2;
+	        if (_elapsed < RepeatTime) return;
 
-	        Instantiate(Package, position, Quaternion.identity);
+	        _elapsed -= RepeatTime;
+	        _index = 0;
 	    }
+
+        var nextSpawnTime = PackageSpawns[_index].TimeBefore;
+	    var nextColor = PackageSpawns[_index].Color;
+
+	    if (!(_elapsed > nextSpawnTime)) return;
+
+	    _elapsed -= nextSpawnTime;
+            
+	    var position = transform.position + transform.up * 2;
+
+	    var package = Instantiate(Package, position, Quaternion.identity);
+
+	    package.GetComponent<Package>().Color = nextColor;
+
+	    _index++;
 	}
     void OnCollisionStay(Collision collision)
     {
