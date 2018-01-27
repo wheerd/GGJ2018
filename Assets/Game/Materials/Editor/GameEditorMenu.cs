4606e8c2f00d9c7b46cf65ed7299eb2e68edad63
Marco Behnke <marco.behnke@innogames.com>
2018-01-27 11:07:08 +0100

    add game play to main scene

---
+++ Assets/Game/Materials/Editor/GameEditorMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEditorMenu : MonoBehaviour
+{
+
+	[MenuItem("Game/Start Game")]
+	static void PlayGame()
+	{
+		if ( EditorApplication.isPlaying == true )
+		{
+			EditorApplication.isPlaying = false;
+			return;
+		}
+
+		EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+		EditorSceneManager.OpenScene("Assets/Game/Scenes/Main.unity");
+		EditorApplication.isPlaying = true;
+	}
+}
--- Assets/Game/Scripts/UI/LevelLostUI.cs
+++ Assets/Game/Scripts/UI/LevelLostUI.cs
@@ -1,15 +1,26 @@
 ﻿using de.deichkrieger.stateMachine;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 public class LevelLostUI : MonoBehaviour {
-
+	[SerializeField] private Text _packets;
+	[SerializeField] private Text _time;
+	
 	[Inject]
 	private GameStartSignal _gameStartSignal;
 	
 	[Inject]
 	private GameHighscoreSignal _gameHighscoreSignal;
+	
+	[Inject] private LevelModel _levelModel;
 
+	void Start()
+	{
+		_packets.text = _levelModel.CorrectPackageCount.ToString() + " / " + _levelModel.ExpectedPackageCount;
+		_time.text = GetTimeFormatted(_levelModel.Timer);
+	}
+	
 	public void StartGame()
 	{
 		_gameStartSignal.Fire();
@@ -18,5 +29,14 @@ public class LevelLostUI : MonoBehaviour {
 	public void ShowHighscore()
 	{
 		_gameHighscoreSignal.Fire();
+	}
+	
+	private string GetTimeFormatted(float time)
+	{
+		var totalSeconds = (int) time;
+		var minutes = totalSeconds / 60;
+		var seconds = totalSeconds % 60;
+
+		return string.Format("{0:D}:{1:00}", minutes, seconds);
 	}
 }
