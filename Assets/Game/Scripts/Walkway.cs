2d93086140ded72bdd96aeb9e055bd023d257a12
Manuel Krebber <admin@wheerd.de>
2018-01-27 01:50:43 +0100

    Add belt animation

--- Assets/Game/Scripts/Walkway.cs
+++ Assets/Game/Scripts/Walkway.cs
@@ -4,6 +4,14 @@ public class Walkway : MonoBehaviour {
 
     public float WalkwaySpeed = 5.0f;
 
+    void Update()
+    {
+        var material = GetComponent<MeshRenderer>().material;
+        var offset = material.GetTextureOffset("_MainTex");
+        offset.y -= Time.deltaTime * WalkwaySpeed / 2;
+        material.SetTextureOffset("_MainTex", offset);
+    }
+
     private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.CompareTag("package"))
