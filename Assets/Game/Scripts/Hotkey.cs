a508ca1a39c5bc5c02a12305942b66f325fcee0d
Manuel Krebber <admin@wheerd.de>
2018-01-28 10:34:07 +0100

    Auto assign hotkeys

---
+++ Assets/Game/Scripts/Hotkey.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public enum Hotkey
+{
+    Auto, Hotkey1, Hotkey2, Hotkey3, Hotkey4, Hotkey5, Hotkey6, Hotkey7, Hotkey8, Hotkey9
+}
+
+public static class HotkeyExtensions
+{
+    public static bool WasPressedThisFrame(this Hotkey hotkey)
+    {
+        return Input.GetButtonDown(hotkey.ToString());
+    }
+
+    public static bool IsPressed(this Hotkey hotkey)
+    {
+        return Input.GetButton(hotkey.ToString());
+    }
+
+    public static bool WasReleasedThisFrame(this Hotkey hotkey)
+    {
+        return Input.GetButtonUp(hotkey.ToString());
+    }
+}
--- Assets/Game/Scripts/Switch.cs
+++ Assets/Game/Scripts/Switch.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using Assets.Game.Scripts;
+using ModestTree;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,13 +10,15 @@ public class Switch : MonoBehaviourWithCursor
     private const float dampening = 0.3f;
     private const float threshold = 1.0f;
 
+    public SwitchBottom Bottom;
+
     private SwitchExit currentExit;
 
     private float currentLevel;
 
     public SwitchExit DefaultExit;
 
-    public string Hotkey = "Hotkey1";
+    public Hotkey Hotkey;
 
     public InputMode InputMode;
 
@@ -24,26 +28,36 @@ public class Switch : MonoBehaviourWithCursor
 
     public SwitchType SwitchType;
 
-    public SwitchBottom Bottom;
-
     private void Start()
     {
         slider.gameObject.SetActive(false);
-        GetComponentInChildren<Text>().text = Hotkey.Substring(Hotkey.Length - 1);
+
+        if (Hotkey == Hotkey.Auto) Hotkey = GetFirstUnusedHotkey();
+
+        var hotkeyName = Hotkey.ToString();
+        GetComponentInChildren<Text>().text = hotkeyName.Substring(hotkeyName.Length - 1);
 
         Bottom.Speed = OutputSpeed;
-        
-        #if UNITY_ANDROID || UNITY_IOS
+
+#if UNITY_ANDROID || UNITY_IOS
         GetComponentInChildren<Text>().transform.parent.gameObject.SetActive(false);
         #endif
-        
+
         currentExit = DefaultExit;
         UpdateRotation();
     }
 
+    private Hotkey GetFirstUnusedHotkey()
+    {
+        var hotkeys = (Hotkey[]) Enum.GetValues(typeof(Hotkey));
+        var usedHotkeys = FindObjectsOfType<Switch>().Select(s => s.Hotkey).ToHashSet();
+
+        return hotkeys.First(h => !usedHotkeys.Contains(h));
+    }
+
     private void FixedUpdate()
     {
-        if (Input.GetButtonDown(Hotkey))
+        if (Hotkey.WasPressedThisFrame())
             switch (InputMode)
             {
                 case InputMode.ButtonMash:
@@ -69,11 +83,11 @@ public class Switch : MonoBehaviourWithCursor
                     break;
             }
 
-        if (Input.GetButtonUp(Hotkey) && InputMode == InputMode.Hold) SetSwitchExit(DefaultExit);
+        if (Hotkey.WasReleasedThisFrame() && InputMode == InputMode.Hold) SetSwitchExit(DefaultExit);
 
         if (currentLevel >= 0)
         {
-            if (!Input.GetButton(Hotkey) || InputMode != InputMode.OneSecond)
+            if (!Hotkey.IsPressed() || InputMode != InputMode.OneSecond)
                 currentLevel -= Time.deltaTime;
             slider.normalizedValue = Math.Max(currentLevel / threshold, 0);
             slider.gameObject.SetActive(true);
@@ -83,10 +97,8 @@ public class Switch : MonoBehaviourWithCursor
             slider.gameObject.SetActive(false);
         }
 
-        if (!Input.GetButton(Hotkey) && InputMode == InputMode.OneSecond && currentExit != DefaultExit && currentLevel <= 0)
-        {
+        if (!Hotkey.IsPressed() && InputMode == InputMode.OneSecond && currentExit != DefaultExit && currentLevel <= 0)
             SetSwitchExit(DefaultExit);
-        }
     }
 
     private void OnMouseDown()
@@ -100,7 +112,7 @@ public class Switch : MonoBehaviourWithCursor
         UpdateRotation();
     }
 
-    void OnValidate()
+    private void OnValidate()
     {
         if (DefaultExit == SwitchExit.Ahead && SwitchType == SwitchType.TwoWay) DefaultExit = SwitchExit.Left;
         currentExit = DefaultExit;
