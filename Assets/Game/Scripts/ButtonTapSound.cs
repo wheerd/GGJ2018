16091dcd6932bcfdbc485eb9d6e1cab89fc8e025
Marco Behnke <marco.behnke@innogames.com>
2018-01-28 13:18:16 +0100

    button click

---
+++ Assets/Game/Scripts/ButtonTapSound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTapSound : MonoBehaviour
+{
+	[SerializeField] AudioClip _audioClip;
+	[SerializeField] [Range(0.0f, 1.0f)] float _volume = 1;
+	AudioSource _audioSource;
+ 
+	void Awake ()
+	{
+		_audioSource = gameObject.AddComponent<AudioSource> ();
+		if (_audioClip != null)
+			_audioSource.clip = _audioClip;
+		_audioSource.playOnAwake = false;
+		_audioSource.volume = _volume;
+		GetComponent<Button> ().onClick.AddListener (() => _audioSource.Play ());
+	}
+}
