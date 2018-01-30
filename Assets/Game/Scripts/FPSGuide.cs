b88465e55f9f480d87bc212f758481ab0883e0cb
Marco Behnke <marco@behnke.biz>
2018-01-30 23:12:09 +0100

    throttle FPS to 30

---
+++ Assets/Game/Scripts/FPSGuide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSGuide : MonoBehaviour {
+
+    [SerializeField]
+    int _maxFps = 30;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = _maxFps;
+    }
+}
