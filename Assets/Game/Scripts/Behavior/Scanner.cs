0a6e2432774120c4c437f97c75f923c1726a0716
Marco Behnke <marco.behnke@innogames.com>
2018-01-30 11:46:04 +0100

    unused code

--- Assets/Game/Scripts/Behavior/Scanner.cs
+++ Assets/Game/Scripts/Behavior/Scanner.cs
@@ -14,24 +14,8 @@ public class Scanner : MonoBehaviour
         var gameObject = other.gameObject;
         if (gameObject.CompareTag(Tags.Package))
         {
-            var position = transform.position;
-            var rigidBody = gameObject.GetComponent<Rigidbody>();
-
-            //rigidBody.velocity = Vector3.zero;
-            //rigidBody.MovePosition(position);
-            //rigidBody.isKinematic = true;
-
-            gameObject.GetComponent<Package>().SetState(PackageState.Normal);
-            //MovePackageToExit(gameObject);
-
+           gameObject.GetComponent<Package>().SetState(PackageState.Normal);
             _playMusicClipSignal.Fire(_packageScanSound);
         }
-    }
-
-      private void MovePackageToExit(GameObject gameObject)
-    {
-        var rigidBody = gameObject.GetComponent<Rigidbody>();
-        rigidBody.isKinematic = false;
-        rigidBody.velocity = transform.forward * -1 * OutputSpeed;
     }
 }
